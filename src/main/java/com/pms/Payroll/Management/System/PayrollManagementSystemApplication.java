c73a2d3c82f93e32cdbee694f92793f40d861b60
Rishi Sarkar <rishisarkar783@gmail.com>
2025-03-08 11:15:31 +0530

    FIRST COMMIT

---
+++ src/main/java/com/pms/Payroll/Management/System/PayrollManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pms.Payroll.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayrollManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayrollManagementSystemApplication.class, args);
+	}
+
+}
